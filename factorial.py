44c6ae520013f1fec5cce81eba907b7b53e022cd
Vedprakash Lilhare <92499422+VedprakashLilhare@users.noreply.github.com>
2023-02-19 14:09:46 +0530

    Add files via upload
    Signed-off-by: Vedprakash Lilhare <92499422+VedprakashLilhare@users.noreply.github.com>

---
+++ factorial.py
@@ -0,0 +1,10 @@
+number = int(input("Enter number for factorial"));
+fact = 1;
+if number<0:
+    print(" Factorial number is not exist for Nagetive Number");
+elif number==0:
+    print("Factorial of 0 is 1");
+else:
+    for i in range(1,number+1):
+        fact= fact*i;
+    print("Factorial of",number,"is=",fact)
